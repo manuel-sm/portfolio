8982430a5f5ba938473c3aab3677747134d801d0
Manuel Santiago <manuel_sm223@hotmail.com>
2021-07-06 13:31:32 -0500

    edit projects page

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,40 +1,40 @@
 import { v4 as uuidv4 } from 'uuid';
-import ProjectImg from '../images/projectImg.png';
-import UTrackerImg from '../images/utracker.jpg';
-import GreenCtgImg from '../images/greenctg.jpg';
-import CoinTrackerImg from '../images/cointracker.jpg';
-import CavinImg from '../images/cavinimg.jpg';
+import disneyImg from '../images/projects/disney.png';
+import easybankImg from '../images/projects/easybank.jpg';
+import gameImg from '../images/projects/game.jpg';
+import phpcrudImg from '../images/projects/phpcrud.png';
+import teslaImg from '../images/projects/tesla.png';
 
 const projects = [
   {
     id: uuidv4(),
-    name: 'U Tracker',
-    desc: 'An application to track your all data from one place. I developed the website and the mobile app',
-    img: UTrackerImg,
+    name: 'Rock-Paper-Scissors game',
+    desc: 'Rock Paper Scissors game using HTML, CSS and Javascript',
+    img: gameImg,
   },
   {
     id: uuidv4(),
-    name: 'Green CTG',
-    desc: 'An app to help people to get an overview of how they can make the city beautiful.',
-    img: GreenCtgImg,
+    name: 'Easybank landing page',
+    desc: 'Landing page coded with HTML, CSS and Javascript completely responsive',
+    img: easybankImg,
   },
   {
     id: uuidv4(),
-    name: 'Coin Tracker',
-    desc: 'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
-    img: CoinTrackerImg,
+    name: 'Disney plus clone',
+    desc: 'Disney plus page using REACT, REDUX, styled components and google authentication',
+    img: disneyImg,
   },
   {
     id: uuidv4(),
-    name: "Cavin's Portfolio",
-    desc: 'A portfolio for Cavin jr. A artist from New york city. The portfolio is made using ReactJs and GatsbyJs.',
-    img: CavinImg,
+    name: "Tesla clone",
+    desc: 'The REACT tesla clone using REDUX and Styled Components',
+    img: teslaImg,
   },
   {
     id: uuidv4(),
-    name: 'Tracking Soft',
-    desc: 'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
-    img: ProjectImg,
+    name: 'PHP CRUD',
+    desc: 'The clasic CRUD program coded with PHP connected to MySQL, and BOOTSTRAP for the styles',
+    img: phpcrudImg,
   },
 ];
 
