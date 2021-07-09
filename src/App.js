c274027e113b1c480ae116c64baa6327010e96a4
Manuel Santiago <manuel_sm223@hotmail.com>
2021-07-08 23:25:32 -0500

    fix smooth scroll

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import NavMenu from './components/NavMenu';
-import ScrollToTop from './components/ScrollToTop'
-/* import SmoothScroll from './components/SmoothScrollbar'
- */import About from './pages/About';
+import SmoothScroll from './components/SmoothScrollbar'
+import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
@@ -14,7 +13,7 @@ export default function App() {
     <>
       <Router>
         <NavMenu />
-        <ScrollToTop/>
+        <SmoothScroll>
         <Switch>
           <Route path="/about">
             <About />
@@ -30,7 +29,7 @@ export default function App() {
           </Route>
         </Switch>
         <Footer />
-        
+      </SmoothScroll>
       </Router>
     </>
   );
--- src/components/SmoothScrollbar.js
+++ src/components/SmoothScrollbar.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
-import Scrollbar from 'smooth-scrollbar-react';
+import { Scrollbar } from 'smooth-scrollbar-react';
 
 function SmoothScroll({ children }) {
   const { pathname } = useLocation();
   const ref = useRef(null);
   useEffect(() => {
-    const { scrollbar } = ref.current;
+    const scrollbar = ref.current;
     scrollbar.setPosition(0, 0);
   }, [pathname]);
 
